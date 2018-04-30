80755ca9cad03c47cb638e5b0735c3eda1aad2d4
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2018-04-30 13:14:33 -0600

    Added a player file with some stubs.

---
+++ GameDev/Model/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GameDev.Model
+{
+    public class Player
+    {
+        public void Initialize()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw()
+        {
+
+        }
+    }
+   
+}
