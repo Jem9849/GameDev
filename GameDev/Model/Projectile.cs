888091c637c81c5814e1c9160db6e93fd364ba6d
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2018-05-01 13:45:33 -0600

    Added data members and getters and setters.

--- GameDev/Model/Projectile.cs
+++ GameDev/Model/Projectile.cs
@@ -12,5 +12,64 @@ namespace GameDev.Model
 		{
 			
 		}
+
+		// Image representing the Projectile
+		private Texture2D texture;
+		public Texture2D Texture
+		{
+			get { return texture; }
+			set { texture = value; }
+		}
+		// Position of the Projectile relative to the upper left side of the screen
+		public Vector2 Position;
+
+		// State of the Projectile
+		private bool active;
+		public bool Active
+		{
+			get { return active; }
+			set { active = value; }
+		}
+
+		// The amount of damage the projectile can inflict to an enemy
+		public int damage;
+		public int Damage
+		{
+			get { return damage; }
+			set { damage = value; }
+		}
+
+		// Represents the viewable boundary of the game
+		private Viewport viewport;
+
+
+		// Get the width of the projectile ship
+		public int Width
+		{
+			get { return Texture.Width; }
+		}
+
+		// Get the height of the projectile ship
+		public int Height
+		{
+			get { return Texture.Height; }
+		}
+
+		// Determines how fast the projectile moves
+		private float projectileMoveSpeed;
+
+
+		public void Initialize()
+		{
+			
+		}
+		public void Update()
+		{
+			
+		}
+		public void Draw()
+		{
+			
+		}
 	}
 }
