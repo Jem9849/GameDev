a4cd25441289c725d920a96b6bffda6adb6be046
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2018-05-01 12:33:37 -0600

    Added another file for parallaxingbackgrounds. It also has stubs.

---
+++ GameDev/View/ParallaxingBackground.cs
@@ -0,0 +1,33 @@
+﻿// ParallaxingBackground.cs
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GameDev.View
+{
+    public class ParallaxingBackground
+    {
+        // The image representing the parallaxing background
+        private Texture2D texture;
+
+        // An array of positions of the parallaxing background
+        private Vector2[] positions;
+
+        // The speed which the background is moving
+        private int speed;
+
+        public void Initialize()
+        {
+
+        }
+        public void Update()
+        {
+
+        }
+        public void Draw()
+        {
+
+        }
+    }
+}
