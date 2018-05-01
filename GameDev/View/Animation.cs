e727d4195381b9fa33b269a271a4719d871e6864
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2018-05-01 12:16:48 -0600

    Added code to the draw, update, and intialize method.

--- GameDev/View/Animation.cs
+++ GameDev/View/Animation.cs
@@ -34,7 +34,7 @@ namespace GameDev.View
         private Rectangle sourceRect = new Rectangle();
 
         // Area of place to display
-        private Rectangle distinationRect = new Rectangle();
+        private Rectangle destinationRect = new Rectangle();
 
         // Frame width
         private int frameWidth;
@@ -71,23 +71,78 @@ namespace GameDev.View
         // Frame width?
         public Vector2 Position;
 
-        public void Initialize()
+        // Use this for initialization
+        public void Initialize(Texture2D texture, Vector2 position, int frameWidth, int frameHeight, int frameCount, int frametime, Color color, float scale, bool looping)
         {
+            // Keep a local copy of the values passed in
+            this.color = color;
+            this.FrameWidth = frameWidth;
+            this.FrameHeight = frameHeight;
+            this.frameCount = frameCount;
+            this.frameTime = frametime;
+            this.scale = scale;
 
+            Looping = looping;
+            Position = position;
+            spriteStrip = texture;
+
+            // Set the time to zero
+            elapsedTime = 0;
+            currentFrame = 0;
+
+            // Set the Animation to active by default
+            Active = true;
         }
 
-
-
-        // Use this for initialization
-        void Start()
+        // Draw the Animation Strip
+        public void Draw(SpriteBatch spriteBatch)
         {
-
+            // Only draw the animation when we are active
+            if (Active)
+            {
+                spriteBatch.Draw(spriteStrip, destinationRect, sourceRect, color);
+            }
         }
 
         // Update is called once per frame
-        void Update()
+        public void Update(GameTime gameTime)
         {
+            // Do not update the game if we are not active
+            if (Active == false)
+            {
+                return;
+            }
+            // Update the elapsed time
+            elapsedTime += (int)gameTime.ElapsedGameTime.TotalMilliseconds;
 
+            // If the elapsed time is larger than the frame time
+            // we need to switch frames
+            if (elapsedTime > frameTime)
+            {
+                // Move to the next frame
+                currentFrame++;
+
+                // If the currentFrame is equal to frameCount reset currentFrame to zero
+                if (currentFrame == frameCount)
+                {
+                    currentFrame = 0;
+                    // If we are not looping deactivate the animation
+                    if (Looping == false)
+                    {
+                        Active = false;
+                    }
+                }
+
+                // Reset the elapsed time to zero
+                elapsedTime = 0;
+            }
+
+            // Grab the correct frame in the image strip by multiplying the currentFrame index by the frame width
+            sourceRect = new Rectangle(currentFrame * FrameWidth, 0, FrameWidth, FrameHeight);
+
+            // Grab the correct frame in the image strip by multiplying the currentFrame index by the frame width
+            destinationRect = new Rectangle((int)Position.X - (int)(FrameWidth * scale) / 2, (int)Position.Y - (int)(FrameHeight * scale) / 2, (int)(FrameWidth * scale), (int)(FrameHeight * scale));
         }
+
     }
 }
